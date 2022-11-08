f0e86409281b8bbf6215431943df6f0423091b48
Joshua Martin <ursa8minority@gmail.com>
2022-11-08 16:56:00 -0700

    Add 3 booleans

--- variables.js
+++ variables.js
@@ -9,3 +9,9 @@ var thought = "What am I going to cook tonight?"
 var age = 34
 var miles = 66
 var players = 3
+
+/// Booleans
+
+var validLicense = true
+var ageEntry = false
+var storeOpen = false
