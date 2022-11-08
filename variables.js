2effa6797f9e14045bcd8034690eadf6fea78317
Joshua Martin <ursa8minority@gmail.com>
2022-11-08 16:51:04 -0700

    Add Numbers

--- variables.js
+++ variables.js
@@ -3,3 +3,9 @@
 var comment = "What a gorgeous sunset!"
 var greeting = "Hello , how are you doing today?"
 var thought = "What am I going to cook tonight?"
+
+/// Number
+
+var age = 34
+var miles = 66
+var players = 3
