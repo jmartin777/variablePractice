b0e9000fc628a7a0d873db0d40bd953e46ffc05f
Joshua Martin <ursa8minority@gmail.com>
2022-11-08 17:09:39 -0700

    Add 7 console logs

--- variables.js
+++ variables.js
@@ -1,17 +1,30 @@
 /// Strings (data type)
-
 var comment = "What a gorgeous sunset!"
 var greeting = "Hello , how are you doing today?"
 var thought = "What am I going to cook tonight?"
 
-/// Number
+/// Reassign "string" variable values
+var comment = "Is anyone out there?"
+var greeting = "Welcome starnger"
+var thought = "I have a pet praying mantis who eats everything."
 
+
+
+/// Number
 var age = 34
 var miles = 66
 var players = 3
 
 /// Booleans
-
 var validLicense = true
 var ageEntry = false
 var storeOpen = false
+
+/// console.logs
+console.log(`my age is ${age}`)
+console.log(`I have walked ${miles} to get here!`)
+console.log(`I was thinking, ${thought}!`)
+console.log(`${greeting}`)
+console.log(`Is there ${players} players?`)
+console.log(storeOpen)
+console.log(ageEntry)
